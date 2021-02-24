9f72f9b25d10a9bf2e4c4582c42e8c086e40fda7
whereislima <reach.lima@gmail.com>
2021-02-24 12:57:46 -0800

    finished exercise

---
+++ find-findindex.js
@@ -0,0 +1,42 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+
+
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(user) {
+    return user.username === username;
+  });
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+
+function removeUser(usersArray, username) {
+  const removeIdx = usersArray.findIndex(function(val) {
+    return val.username === username;
+  });
+  // if statement used from solution, return undefined
+  if(removeIdx === -1) return; 
+  // added the index 0 from solution, without the [0] it will return an array instead of a single element 
+  return usersArray.splice(removeIdx, 1)[0];
+}
